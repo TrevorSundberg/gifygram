96854d152fc45735740780404bcbbff40bd01b53
Trevor Sundberg <tsundberg@s2.systems>
2019-12-12 09:49:02 -0800

    Added a video element that seeks the animation

--- src/index.tsx
+++ src/index.tsx
@@ -9,28 +9,33 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-const scene = new Scene(
-  {
-    "#test": {
-      0: "left: 0px; top: 0px; transform: translate(0px);",
-      1: {
-        left: "100px",
-        top: "0px",
-        transform: "translate(50px)"
-      },
-      2: {
-        left: "200px",
-        top: "100px",
-        transform: {
-          translate: "100px"
-        }
+const scene = new Scene({
+  "#test": {
+    0: "left: 0px; top: 0px; transform: translate(0px);",
+    1: {
+      left: "100px",
+      top: "0px",
+      transform: "translate(50px)"
+    },
+    2: {
+      left: "200px",
+      top: "100px",
+      transform: {
+        translate: "100px"
       }
     }
-  },
-  {
-    easing: "ease-in-out",
-    selector: true
   }
-);
+}, {
+  easing: "ease-in-out",
+  selector: true
+});
 
-scene.setTime(0.5);
+const video = document.getElementById("video") as HTMLVideoElement;
+
+const update = () => {
+  requestAnimationFrame(() => {
+    update();
+  });
+  scene.setTime(video.currentTime);
+};
+update();
