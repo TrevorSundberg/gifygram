b46ebf953b62412493dd3fdf45d4ce3f991e52f3
Trevor Sundberg <tsundberg@s2.systems>
2019-12-14 12:56:06 -0800

    Attempting to add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  devtool: "source-map",
   entry: "./src/index.tsx",
   module: {
     rules: [
