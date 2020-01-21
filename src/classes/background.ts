822f428461a2774e93f394a335dae75de4071aed
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-01-21 09:22:39 -0800

    Use fixed size background canvas for perf

--- src/classes/background.ts
+++ src/classes/background.ts
@@ -4,23 +4,20 @@ import {FRAME_TIME} from "./utility";
 export class Background {
   public constructor (parent: HTMLElement, video: HTMLVideoElement) {
     const canvas = document.createElement("canvas");
+    canvas.width = 256;
+    canvas.height = 256;
     canvas.className = "background";
 
     const context = canvas.getContext("2d");
     const drawVideo = () => {
-      context.filter = "blur(5vmin)";
-      context.drawImage(video, 0, 0, window.innerWidth, window.innerHeight);
+      context.filter = "blur(10px)";
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
     };
-    const updateDimensions = () => {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      drawVideo();
-    };
-    updateDimensions();
+    drawVideo();
     parent.prepend(canvas);
 
-    window.addEventListener("resize", updateDimensions);
+    window.addEventListener("resize", drawVideo);
     video.addEventListener("seeked", drawVideo);
-    setInterval(drawVideo, FRAME_TIME * 1000);
+    setInterval(drawVideo, FRAME_TIME * 1000 * 2);
   }
 }
