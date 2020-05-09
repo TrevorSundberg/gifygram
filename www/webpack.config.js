ad3af1676bc1c137dd197afca2c96be150d76937
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-05-09 02:21:29 -0700

    Formatted the webpack config

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,29 +1,37 @@
-const path = require('path')
-const webpack = require('webpack')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
 
-const mode = process.env.NODE_ENV || 'production'
+const mode = process.env.NODE_ENV || "production";
 
 module.exports = {
-  output: {
-    filename: `worker.${mode}.js`,
-    path: path.join(__dirname, 'dist'),
-  },
-  devtool: 'source-map',
+  devtool: "source-map",
   mode,
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    plugins: [],
-  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
-          transpileOnly: true,
+          transpileOnly: true
         },
+        test: /\.tsx?$/u
       },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
-    ],
+      {
+        enforce: "pre",
+        loader: "source-map-loader",
+        test: /\.js$/u
+      }
+    ]
   },
-}
+  output: {
+    filename: `worker.${mode}.js`,
+    path: path.join(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".js"
+    ],
+    plugins: []
+  }
+};
