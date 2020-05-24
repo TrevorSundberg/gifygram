bd5ee8c6dfe8054c161c15dc10fd9333d360e315
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-05-24 16:34:41 -0700

    Move Threads to being a functional component

--- client/src/www/threads.tsx
+++ client/src/www/threads.tsx
@@ -1,71 +1,65 @@
 import {API_THREAD_LIST, ReturnedPost} from "../../../common/common";
-import {AbortablePromise, abortableJsonFetch, cancel, makeUrl} from "../shared/shared";
 import {Card, CardMedia} from "@material-ui/core";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {abortableJsonFetch, cancel, makeUrl} from "../shared/shared";
 
 interface ThreadsProps {
   history: import("history").History;
 }
 
-interface ThreadsState {
-  threads: ReturnedPost[];
-}
+export const Threads: React.FC<ThreadsProps> = ({history}: ThreadsProps) => {
+  const [
+    threads,
+    setThreads
+  ] = useState<ReturnedPost[]>([]);
 
-export class Threads extends React.Component<ThreadsProps, ThreadsState> {
-  public state: ThreadsState = {
-    threads: []
-  }
+  useEffect(() => {
+    const threadListFetch = abortableJsonFetch<ReturnedPost[]>(API_THREAD_LIST);
+    threadListFetch.then((threadList) => {
+      if (threadList) {
+        setThreads(threadList);
+      }
+    });
 
-  private threadListFetch: AbortablePromise<ReturnedPost[]>;
+    return () => {
+      cancel(threadListFetch);
+    };
+  }, []);
 
-  public async componentDidMount () {
-    this.threadListFetch = abortableJsonFetch<ReturnedPost[]>(API_THREAD_LIST);
-    const threads = await this.threadListFetch;
-    if (threads) {
-      this.setState({threads});
-    }
-  }
-
-  public componentWillUnmount () {
-    cancel(this.threadListFetch);
-  }
-
-  public render () {
-    return (
-      <div style={{
-        columnCount: 4,
-        columnWidth: "150px",
-        columnGap: "10px"
-      }}>
-        {this.state.threads.map((thread) => <Card
-          key={thread.id}
-          style={{
-            breakInside: "avoid"
-          }}
-          onClick={() => {
-            this.props.history.push(`/thread?threadId=${thread.id}`);
-          }}>
-          {thread.username}
-          <CardMedia
-            component="video"
-            src={makeUrl("/api/animation/video", {id: thread.id})}
-            title="Contemplative Reptile"
-          />
-          <video
-            style={{width: "100%"}}
-            muted
-            loop
-            onMouseEnter={(event) => (event.target as HTMLVideoElement).play().catch(() => 0)}
-            onMouseLeave={(event) => (event.target as HTMLVideoElement).pause()}
-            onTouchStart={(event) => (event.target as HTMLVideoElement).play().catch(() => 0)}
-            onTouchEnd={(event) => (event.target as HTMLVideoElement).pause()}
-            src={makeUrl("/api/animation/video", {id: thread.id})}>
-          </video>
-          <div
-            style={{padding: "6px", paddingTop: "0px"}}>
-            TITLE: {thread.title}
-          </div>
-        </Card>)}
-      </div>);
-  }
-}
+  return (
+    <div style={{
+      columnCount: 4,
+      columnWidth: "150px",
+      columnGap: "10px"
+    }}>
+      {threads.map((thread) => <Card
+        key={thread.id}
+        style={{
+          breakInside: "avoid"
+        }}
+        onClick={() => {
+          history.push(`/thread?threadId=${thread.id}`);
+        }}>
+        {thread.username}
+        <CardMedia
+          component="video"
+          src={makeUrl("/api/animation/video", {id: thread.id})}
+          title="Contemplative Reptile"
+        />
+        <video
+          style={{width: "100%"}}
+          muted
+          loop
+          onMouseEnter={(event) => (event.target as HTMLVideoElement).play().catch(() => 0)}
+          onMouseLeave={(event) => (event.target as HTMLVideoElement).pause()}
+          onTouchStart={(event) => (event.target as HTMLVideoElement).play().catch(() => 0)}
+          onTouchEnd={(event) => (event.target as HTMLVideoElement).pause()}
+          src={makeUrl("/api/animation/video", {id: thread.id})}>
+        </video>
+        <div
+          style={{padding: "6px", paddingTop: "0px"}}>
+          TITLE: {thread.title}
+        </div>
+      </Card>)}
+    </div>);
+};
