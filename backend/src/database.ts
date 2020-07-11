5d06e16efdd0e73510058485a0420ec201e528ee
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-07-11 14:14:55 -0700

    Change style in database file

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,16 +1,16 @@
 import {StoredUser} from "../../common/common";
 
-const KEY_AUTH_GOOGLE = "auth:google";
-
 export type UserId = string;
 export type JWKS = {keys: JWKRSA[]};
 
-export const dbGetCachedJwksGoogle = async (): Promise<JWKS | null> => db.get<JWKS>(KEY_AUTH_GOOGLE, "json");
+const KEY_AUTH_GOOGLE = "auth:google";
+const dbkeyUser = (userId: UserId) => `user:${userId}`;
+
+export const dbGetCachedJwksGoogle = async (): Promise<JWKS | null> =>
+  db.get<JWKS>(KEY_AUTH_GOOGLE, "json");
 
 export const dbPutCachedJwksGoogle = async (jwks: JWKS, expiration: number): Promise<void> =>
   db.put(KEY_AUTH_GOOGLE, JSON.stringify(jwks), {expiration});
-
-const dbkeyUser = (userId: UserId) => `user:${userId}`;
 
 export const dbGetUser = async (userId: UserId): Promise<StoredUser | null> =>
   db.get<StoredUser>(dbkeyUser(userId), "json");
