32adb1f19a4c44affc13771644a80d33c6dd563c
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-07-19 21:12:38 -0700

    Support transparent avatars with background color

--- frontend/src/page/userAvatar.tsx
+++ frontend/src/page/userAvatar.tsx
@@ -2,6 +2,7 @@ import {API_PROFILE_AVATAR} from "../../../common/common";
 import Avatar from "@material-ui/core/Avatar";
 import React from "react";
 import {makeServerUrl} from "../shared/shared";
+import {theme} from "./style";
 
 interface UserAvatarProps {
   username: string;
@@ -9,8 +10,11 @@ interface UserAvatarProps {
 }
 
 export const UserAvatar: React.FC<UserAvatarProps> = (props) =>
-  <Avatar alt={props.username} src={props.avatarId
-    ? makeServerUrl(API_PROFILE_AVATAR, {avatarId: props.avatarId})
-    : null}>
+  <Avatar
+    style={{backgroundColor: theme.palette.divider}}
+    alt={props.username}
+    src={props.avatarId
+      ? makeServerUrl(API_PROFILE_AVATAR, {avatarId: props.avatarId})
+      : null}>
     {props.username.slice(0, 1).toUpperCase()}
   </Avatar>;
