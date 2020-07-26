c7e766abe3600d2e145c46d7c85a9b7a5d69da44
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-07-25 18:41:12 -0700

    Clean webpack output for frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const uuidv4 = require("uuid/v4");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
   devServer: {
@@ -82,6 +83,7 @@ module.exports = {
     )
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.htm",
       title: require("./title")
