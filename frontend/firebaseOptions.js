7d7069b1208435cc519318eb66af640d14b27e22
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-07-26 16:15:22 -0700

    Add projectId to firebase options (for firestore)

--- frontend/firebaseOptions.js
+++ frontend/firebaseOptions.js
@@ -1,4 +1,5 @@
 module.exports = {
   apiKey: "AIzaSyBv0-S-2rb8m-JKs8TwF-T3uKA_knv9fpQ",
-  authDomain: "gifygram-site.firebaseapp.com"
+  authDomain: "gifygram-site.firebaseapp.com",
+  projectId: "gifygram-site"
 };
