a00b24d2a94380b6c5665dca56a8652e23ee1f9b
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-08-01 21:36:53 -0700

    Integrate Firebase analytics

--- frontend/firebaseOptions.js
+++ frontend/firebaseOptions.js
@@ -1,5 +1,10 @@
 module.exports = {
   apiKey: "AIzaSyBv0-S-2rb8m-JKs8TwF-T3uKA_knv9fpQ",
   authDomain: "gifygram-site.firebaseapp.com",
-  projectId: "gifygram-site"
+  databaseURL: "https://gifygram-site.firebaseio.com",
+  projectId: "gifygram-site",
+  storageBucket: "gifygram-site.appspot.com",
+  messagingSenderId: "780323380600",
+  appId: "1:780323380600:web:d8b1b193b0ef86e9fafc8d",
+  measurementId: "G-E8PQ0JL7W3"
 };
--- frontend/src/shared/firebase.ts
+++ frontend/src/shared/firebase.ts
@@ -1,11 +1,13 @@
 import firebase from "firebase/app";
 // eslint-disable-next-line sort-imports
 import "firebase/auth";
+import "firebase/analytics";
 import "firebase/firestore";
 import {isDevEnvironment} from "./shared";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 firebase.initializeApp(require("../../firebaseOptions"));
+firebase.analytics();
 
 if (isDevEnvironment()) {
   firebase.firestore().settings({
